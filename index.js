2e7608a16f4ecd1e58a682814147052628f1b52a
Thomas E Beach <thomas.beach@flatironschool.com>
2018-08-28 23:33:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,32 +29,32 @@ function renderBooks(json) {
   })
 }
 
-// function fetchHouses() {
-//   console.log("== fetchHouses ==");
-//
-//   fetch('https://www.anapioficeandfire.com/api/houses')
-// 	  .then(function(response) {
-//       console.log("-- houses response --");
-// 	    console.log("response:", response);
-// 	    return response.json();
-// 	  })
-// 	  .then(function(jsonData) {
-//       console.log("-- houses json --");
-// 	    console.log("jsonData:", jsonData);
-//       console.log("jsonData.length:", jsonData.length);
-//       renderHouses(jsonData);
-// 	  });
-//
-//   function renderHouses(json) {
-//     console.log("== renderHouses ==");
-//     const main = document.querySelector('main')
-//     json.forEach(house => {
-//       console.log("house:", house);
-//       // const h4 = document.createElement('h4')
-//       // h4.innerHTML = `<h4>${house.name}</h4>`
-//       // main.appendChild(h4)
-//     })
-//   }
+function fetchHouses() {
+  console.log("== fetchHouses ==");
+
+  fetch('https://www.anapioficeandfire.com/api/houses')
+	  .then(function(response) {
+      console.log("-- houses response --");
+	    console.log("response:", response);
+	    return response.json();
+	  })
+	  .then(function(jsonData) {
+      console.log("-- houses json --");
+	    console.log("jsonData:", jsonData);
+      console.log("jsonData.length:", jsonData.length);
+      renderHouses(jsonData);
+	  });
+
+  function renderHouses(json) {
+    console.log("== renderHouses ==");
+    const main = document.querySelector('main')
+    json.forEach(house => {
+      console.log("house:", house);
+      // const h4 = document.createElement('h4')
+      // h4.innerHTML = `<h4>${house.name}</h4>`
+      // main.appendChild(h4)
+    })
+  }
 
 // }
 
